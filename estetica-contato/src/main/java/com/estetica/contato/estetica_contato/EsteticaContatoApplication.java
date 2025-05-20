71b2e64f030e449f34e8cab71acdffe2b7c9a55a
Matheus Foganholi Bispo Dantas <matheus.foganholi67@gmail.com>
2025-05-20 15:56:04 -0300

    alocação do spring boot no projeto e banco de dados

---
+++ estetica-contato/src/main/java/com/estetica/contato/estetica_contato/EsteticaContatoApplication.java
@@ -0,0 +1,13 @@
+package com.estetica.contato.estetica_contato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsteticaContatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsteticaContatoApplication.class, args);
+	}
+
+}
---
+++ estetica-contato/src/test/java/com/estetica/contato/estetica_contato/EsteticaContatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estetica.contato.estetica_contato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsteticaContatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
