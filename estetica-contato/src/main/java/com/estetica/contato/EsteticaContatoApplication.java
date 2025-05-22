ece110bdb365f235e512c86b5f8b4414f10cc578
Matheus Foganholi Bispo Dantas <matheus.foganholi67@gmail.com>
2025-05-22 19:05:35 -0300

    teste spring boot cliente

---
+++ estetica-contato/src/main/java/com/estetica/contato/EsteticaContatoApplication.java
@@ -0,0 +1,11 @@
+package com.estetica.contato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsteticaContatoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EsteticaContatoApplication.class, args);
+    }
+}
